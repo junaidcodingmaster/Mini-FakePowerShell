cf7009fc9143b406c74e05c193e0cef1e4b20322
Md . Junaid <72298298+junaidcodingmaster@users.noreply.github.com>
2022-08-07 01:53:48 +0530

    Add files via upload

---
+++ mini-fakepowershell.sh
@@ -0,0 +1,9 @@
+echo "FakePowerShell is unable to do some commmands in linux."
+echo 
+echo "Mini-FakePowerShell is creating a folder for launch FakePowerShell ..."
+
+pip install -r ./Fake-PowerShell-0.1/requirements.txt
+
+clear
+
+python ./Fake-PowerShell-0.1/powerShell.py
